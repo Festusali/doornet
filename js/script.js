0ef7533cb9d8824b2c4c5a7605a5e2d49d034253
Festus Ali <festusali@gmail.com>
2025-06-05 09:59:53 +0100

    Implemented Order Form Design & Added Favicon

--- js/script.js
+++ js/script.js
@@ -2,29 +2,29 @@
 
 // Assigns corresponding value to price based on Net quantity
 function checkForm() {
-    orderQty = document.orderForm.quantity
-    if (orderQty.value == 1) {
-        document.orderForm.price.value = "₦25,500";
-        return true;
-    }
-    if (orderQty.value == 2) {
-        document.orderForm.price.value = "₦46,000";
-        return true;
-    }
-    if (orderQty.value == 3) {
-        document.orderForm.price.value = "₦67,500";
-        return true;
-    }
-    if (orderQty.value == 4) {
-        document.orderForm.price.value = "₦88,000";
-        return true;
-    }
-    if (orderQty.value == 5) {
-        document.orderForm.price.value = "₦107,500";
-        return true;
-    }
-    if (orderQty.value == 6) {
-        document.orderForm.price.value = "₦126,000";
-        return true;
-    }
+  orderQty = document.orderForm.quantity;
+  if (orderQty.value == 1) {
+    document.orderForm.price.value = "₦25,500";
+    return true;
+  }
+  if (orderQty.value == 2) {
+    document.orderForm.price.value = "₦46,000";
+    return true;
+  }
+  if (orderQty.value == 3) {
+    document.orderForm.price.value = "₦67,500";
+    return true;
+  }
+  if (orderQty.value == 4) {
+    document.orderForm.price.value = "₦88,000";
+    return true;
+  }
+  if (orderQty.value == 5) {
+    document.orderForm.price.value = "₦107,500";
+    return true;
+  }
+  if (orderQty.value == 6) {
+    document.orderForm.price.value = "₦126,000";
+    return true;
+  }
 }
