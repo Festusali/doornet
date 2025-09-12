91a0103eb308d8e2a59cdd30bff656d8b9cf851c
Festus Ali <festusali@gmail.com>
2025-09-12 15:42:48 +0100

    Added Product Prices

--- cacatin/js/script.js
+++ cacatin/js/script.js
@@ -4,43 +4,19 @@
 function checkForm() {
   orderQty = document.orderForm.quantity;
   if (orderQty.value == 1) {
-    document.orderForm.price.value = "₦25,000";
+    document.orderForm.price.value = "₦16,000";
     return true;
   }
   if (orderQty.value == 2) {
-    document.orderForm.price.value = "₦46,000";
+    document.orderForm.price.value = "₦20,000";
     return true;
   }
   if (orderQty.value == 3) {
-    document.orderForm.price.value = "₦69,000";
+    document.orderForm.price.value = "₦25,000";
     return true;
   }
   if (orderQty.value == 4) {
-    document.orderForm.price.value = "₦92,000";
-    return true;
-  }
-  if (orderQty.value == 5) {
-    document.orderForm.price.value = "₦115,000";
-    return true;
-  }
-  if (orderQty.value == 6) {
-    document.orderForm.price.value = "₦138,000";
-    return true;
-  }
-  if (orderQty.value == 7) {
-    document.orderForm.price.value = "₦161,000";
-    return true;
-  }
-  if (orderQty.value == 8) {
-    document.orderForm.price.value = "₦184,000";
-    return true;
-  }
-  if (orderQty.value == 9) {
-    document.orderForm.price.value = "₦207,000";
-    return true;
-  }
-  if (orderQty.value == 10) {
-    document.orderForm.price.value = "₦230,000";
+    document.orderForm.price.value = "₦30,000";
     return true;
   }
 }
