24355d148fb680bde59a3c524c62fe9764fcd142
Festus Ali <festusali@gmail.com>
2025-11-23 17:30:11 +0100

    Change Price On Door Net Website 15th

--- js/script.js
+++ js/script.js
@@ -4,43 +4,27 @@
 function checkForm() {
   orderQty = document.orderForm.quantity;
   if (orderQty.value == 1) {
-    document.orderForm.price.value = "₦20,000";
+    document.orderForm.price.value = "₦25,000";
     return true;
   }
   if (orderQty.value == 2) {
-    document.orderForm.price.value = "₦40,000";
+    document.orderForm.price.value = "₦45,000";
     return true;
   }
   if (orderQty.value == 3) {
-    document.orderForm.price.value = "₦60,000";
+    document.orderForm.price.value = "₦66,000";
     return true;
   }
   if (orderQty.value == 4) {
-    document.orderForm.price.value = "₦80,000";
+    document.orderForm.price.value = "₦86,000";
     return true;
   }
   if (orderQty.value == 5) {
-    document.orderForm.price.value = "₦100,000";
+    document.orderForm.price.value = "₦105,000";
     return true;
   }
   if (orderQty.value == 6) {
     document.orderForm.price.value = "₦120,000";
-    return true;
-  }
-  if (orderQty.value == 7) {
-    document.orderForm.price.value = "₦140,000";
-    return true;
-  }
-  if (orderQty.value == 8) {
-    document.orderForm.price.value = "₦160,000";
-    return true;
-  }
-  if (orderQty.value == 9) {
-    document.orderForm.price.value = "₦180,000";
-    return true;
-  }
-  if (orderQty.value == 10) {
-    document.orderForm.price.value = "₦200,000";
     return true;
   }
 }
