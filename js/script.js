46bf441ae42dff205756572d0756b75576dfc24a
Festus Ali <festusali@gmail.com>
2026-08-21 09:01:35 +0100

    Change Price On Door Net Website 18th

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function disableOrderButtons() {
 // Assigns corresponding value to price based on Net quantity
 function getPrice(quantity) {
   const prices = {
-    1: "₦27,000",
+    1: "₦25,000",
     2: "₦48,000",
     3: "₦70,500",
     4: "₦92,000",
